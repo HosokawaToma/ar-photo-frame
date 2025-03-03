f60ea57b13a122982a7e01fe6d8c9bb6c9e9ebbf
HosokawaToma <gp22a160@oecu.jp>
2025-03-03 14:08:01 +0900

    fix: SVGアイコンの修正

--- src/components/CameraToggleFacingButton.tsx
+++ src/components/CameraToggleFacingButton.tsx
@@ -1,15 +1,11 @@
 import React from "react";
-import { IconButton } from "@mui/material";
-import PartyModeIcon from "@mui/icons-material/PartyMode";
-import styles from "@/styles/CameraToggleFacingButton.module.css";
+import styles from "@/styles/cameraToggleFacingButton.module.css";
 
 const CameraToggleFacingButton = ({ onToggle }: { onToggle: () => void }) => {
   return (
-    <div className={styles.buttonWrapper}>
-      <IconButton onClick={onToggle} className={styles.toggleButton}>
-        <PartyModeIcon className={styles.icon} sx={{ fontSize: "3rem" }} />
-      </IconButton>
-    </div>
+    <button onClick={onToggle} className={styles.toggleButton}>
+      <div className={styles.icon} />
+    </button>
   );
 };
 
