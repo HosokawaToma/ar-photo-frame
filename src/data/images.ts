e64ff27946abcebca2348d5d38657a663a143388
HosokawaToma <gp22a160@oecu.jp>
2025-03-07 22:33:22 +0900

    fix: オーバレイ画像が取得出来ない問題を解消
    - オーバレイ画像を公開されたストレージに設置
    - URLを調整

--- src/data/images.ts
+++ src/data/images.ts
@@ -1,6 +1,30 @@
 export const imagesData = [
-  { id: "faculty_of_architectural_design", fileUrl: "https://oecu-ar-photo-frame-bucket.s3.us-east-1.amazonaws.com/faculty_of_architectural_design.png", width: 1280 , height: 960, type: "png" },
-  { id:  "faculty_of_comprehensive_informatics", fileUrl: "https://oecu-ar-photo-frame-bucket.s3.us-east-1.amazonaws.com/faculty_of_comprehensive_informatics.png", width: 1280 , height: 960, type: "png" },
-  { id:  "faculty_of_information_and_communication_engineering", fileUrl: "https://oecu-ar-photo-frame-bucket.s3.us-east-1.amazonaws.com/faculty_of_information_and_communication_engineering.png", width: 1280 , height: 960, type: "png" },
-  { id:  "faculty_of_medical_and_health_sciences", fileUrl: "https://oecu-ar-photo-frame-bucket.s3.us-east-1.amazonaws.com/faculty_of_medical_and_health_sciences.png", width: 1280 , height: 960, type: "png" },
-]
+  {
+    id: "faculty_of_architectural_design",
+    fileUrl: "https://d3ebzsyq2w7mpl.cloudfront.net/faculty_of_architectural_design.png",
+    width: 1280,
+    height: 960,
+    type: "png",
+  },
+  {
+    id: "faculty_of_comprehensive_informatics",
+    fileUrl: "https://d3ebzsyq2w7mpl.cloudfront.net/faculty_of_comprehensive_informatics.png",
+    width: 1280,
+    height: 960,
+    type: "png",
+  },
+  {
+    id: "faculty_of_information_and_communication_engineering",
+    fileUrl: "https://d3ebzsyq2w7mpl.cloudfront.net/faculty_of_information_and_communication_engineering.png",
+    width: 1280,
+    height: 960,
+    type: "png",
+  },
+  {
+    id: "faculty_of_medical_and_health_sciences",
+    fileUrl: "https://d3ebzsyq2w7mpl.cloudfront.net/faculty_of_medical_and_health_sciences.png",
+    width: 1280,
+    height: 960,
+    type: "png",
+  },
+];
